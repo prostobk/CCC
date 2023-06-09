f05a083fa6a2ab28308c8fd140c1c519acce9e26
Nikolay <prostobk18@gmail.com>
2023-06-09 23:13:35 +0300

    Первый семинар

---
+++ Seminar1/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число: ");
+string? numberStringOne = Console.ReadLine();
+
+Console.WriteLine("Введите первое число: ");
+string? numberStringTwo = Console.ReadLine();
+
+int numberStringOne = Convert.ToInt32(numberStringOne);
+int numberStringOne = Convert.ToInt32(numberStringOne);
+
+if (numberStringTwo == 1)
+{
+    
+}
---
+++ Seminar1/Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿int numberDay;
+
+Console.WriteLine("Введите число: ");
+string? numberString = Console.ReadLine();
+int numberInt = Convert.ToInt32(numberString);
+
+if (numberInt == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (numberInt == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if (numberInt == 3)
+{
+    Console.WriteLine("Среда");
+}
+if (numberInt == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if (numberInt == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if (numberInt == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if (numberInt == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+if (numberInt > 7 || numberInt < 1)
+{
+    Console.WriteLine("Такого дня нет");
+}
