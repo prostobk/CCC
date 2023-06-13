325e978c76f4f2a61e19f0526d5cfd62cdb161d4
Nikolai Baikalov <prostobk18@gmail.com>
2023-06-13 21:00:47 +0300

    Решена вторая задача семинара

---
+++ Seminar2/Task2/Program.cs
@@ -0,0 +1,7 @@
+﻿int number = Random.Shared.Next(100, 1000);
+Console.WriteLine(number);
+int minDigit = number / 100;
+Console.WriteLine(minDigit);
+int maxDigit = number % 10;
+Console.WriteLine(maxDigit);
+Console.WriteLine(minDigit * 10 + maxDigit);
