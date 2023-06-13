37873d4eccb83569309781eb81b9e04a5315a764
Nikolai Baikalov <prostobk18@gmail.com>
2023-06-13 20:50:14 +0300

    Изменения

---
+++ Seminar2/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿int number = Random.Shared.Next(10, 100);
+Console.WriteLine(number);
+int lastDigit = number % 10;
+int firstDigit = number / 10;
+
+if(firstDigit > lastDigit)
+{
+    Console.WriteLine(firstDigit);
+}
+else if(lastDigit > firstDigit)
+{
+    Console.WriteLine(lastDigit);
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
