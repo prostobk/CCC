22ce2a1d09fbc9ab0b5d986da0058486d8d81fa6
Nikolai Baikalov <prostobk18@gmail.com>
2023-06-14 17:14:29 +0300

    ДЗ второго семинара выполнено

--- Seminar2/Task13HomeWork/Program.cs
+++ Seminar2/Task13HomeWork/Program.cs
@@ -2,3 +2,15 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100 || number > 999)
+{
+    Console.WriteLine("Вы ввели не трёхзначное число");
+}
+else
+{
+    int num1 = number / 10;
+    int num2 = num1 % 10;
+    Console.WriteLine(num2);
+}
