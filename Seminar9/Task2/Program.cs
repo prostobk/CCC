953471e8bc76864d1f6e41acd339febc6ff81218
Nikolay <prostobk18@gmail.com>
2023-07-07 21:57:42 +0300

    9-й семинар завершён

---
+++ Seminar9/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+int number = 10;
+
+void Perechislenie(int lastNumber)
+{
+    if (lastNumber <= 0) return;
+    Perechislenie (lastNumber - 1);
+    Console.Write (lastNumber + ", " );
+}
+Perechislenie(number);
---
+++ Seminar9/Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int number = 145;
+
+int SummaChisel(int chislo)
+{
+    if (chislo <= 0) return 0;
+    return SummaChisel(chislo / 10) + (chislo % 10);
+}
+
+int result = SummaChisel(number);
+Console.WriteLine(result);
